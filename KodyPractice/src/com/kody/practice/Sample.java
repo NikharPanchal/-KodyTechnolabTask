284d41188ab9492773a4597045d83ff9d10d5952
Nikhar <Nikhar@LAPTOP-28NQD5J0>
2022-06-01 16:47:53 +0530

    Change Sample file

---
+++ KodyPractice/src/com/kody/practice/Sample.java
@@ -0,0 +1,13 @@
+package com.kody.practice;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+	}
+	public static void main(String args) {
+		System.out.println("World");
+	}
+}
+
