6e11c12643b54d21577845b21102c45dd253b420
Nikhar <Nikhar@LAPTOP-28NQD5J0>
2022-06-22 18:14:12 +0530

    NullPointer

--- KodyPractice/src/com/kody/practice/exception/IllegalException.java
+++ KodyPractice/src/com/kody/practice/exception/IllegalException.java
@@ -1,22 +1,27 @@
 package com.kody.practice.exception;
 
 /**
- * @objective Example of illegalException
+ * @objective Example of illegalException.. Only checked exceptions are required
+ *            to be thrown using the throws keyword. Unchecked exceptions don’t
+ *            need to be thrown or handled explicitly in code.
  * @Developer Nikhar
  * @Date 10-Jun-2022
  */
 public class IllegalException {
 
-	static void throwOne() throws IllegalAccessException {
+	static void throwOne() {
 		System.out.println("Inside throwOne.");
-		throw new IllegalAccessException("demo");
+
+		throw new ArithmeticException("Arithmatic Exceptions");
+
 	}
 
 	public static void main(String args[]) {
+
 		try {
 			throwOne();
-		} catch (IllegalAccessException e) {
-			System.out.println("Caught " + e);
+		} catch (ArithmeticException e) {
+			System.out.println(e.getMessage());
 		}
 	}
 }
--- KodyPractice/src/com/kody/practice/exception/TaskExampleException.java
+++ KodyPractice/src/com/kody/practice/exception/TaskExampleException.java
@@ -1,9 +1,27 @@
 package com.kody.practice.exception;
-
 /**
  * @objective This is the first demo of the Exception
  * @Developer Nikhar
- * @Date 08-Jun-2022
+ * @Date 08-Jun-2022 
+ * 		 Note :Only checked exceptions are required to be thrown
+ *       using the throws keyword. Unchecked exceptions don’t need to be thrown
+ *       or handled explicitly in code.
+ *       
+ *       Checked Exception  
+ *       -ClassNotFoundException
+ *       -InterruptedException
+ *       -IOException
+ *       -InstantiationException
+ *       -SQLException
+ *       -FilNot Found Exception
+ *       -IllegalAccessException
+ *       
+ *       UnCheckedException 
+ *       -ArithmaticException
+ *       -ClassCastException
+ *       -NullPointerExcepttion
+ *       -InputMisMatchedException
+ *       -ArrayIndexOutOufBoundException
  */
 public class TaskExampleException {
 
